9e59d05f4420e2245933e539b91d02a50bf0ddc0
Arthur40480 <arthurgibert@free.fr>
2022-10-10 09:42:19 +0200

    add Mongoose unique validator

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,15 +2,27 @@
 const mongoose = require('mongoose');
 
 /**
+ * On rajoute ce validateur comme plugin à notre shéma
+ */
+const uniqueValidator = require('mongoose-unique-validator');
+
+/**
  * Utilisation de la fonction .Schema du package mongoose pour
  * créer un shéma de données qui seront les données utilisateurs
  * pour pouvoir ce connecter.
  */
 const schemaUsers = mongoose.Schema({
-    // On passe la configuration 'required' pour dire que ce champ est requis.
-    email: { type: String, required: true }, // adresse e-mail de l'utilisateur [unique]
+    /**
+     * On passe la configuration 'required' pour dire que ce champ est requis.
+     * On passe la configuration 'unique' pour pas que l'adresse mail soit utilisée
+     * plusieurs fois pour plusieurs compte différents.
+     */
+    email: { type: String, required: true, unique: true }, // adresse e-mail de l'utilisateur [unique]
     password: { type: String, required: true }, // mot de passe de l'utilisateur haché
 });
+
+// On applique ce validator à notre schéma avant d'en faire un model
+schemaUsers.plugin(uniqueValidator);
 
 /**
  * Pour exploiter ce schéma comme model pour cela, on vas utiliser
